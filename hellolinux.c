49e05ac06727a31e0061d73aeda44b34856758d5
Nicholas <blockpit@gmail.com>
2018-01-13 16:44:42 +1000

    Create hellolinux.c

---
+++ hellolinux.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int fibo()
+{
+  return 0;
+}
+
+
+void main()
+{
+  printf("Hello Linux!");
+  int test = 5;
+  /*while(1)
+    {
+      printf("Hello");
+      }*/
+  double yoyo = 10; 
+  printf("%d\n",fibo());
+}
