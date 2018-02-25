3105a5ab804216335a0470235ae87b16e688b3c0
Nicholas <blockpit@gmail.com>
2018-02-25 15:15:15 +1000

    Add files via upload

--- python/fourth.py
+++ python/fourth.py
@@ -25,6 +25,8 @@ print(nand(bools1,bools2))
 
 print('----------------- NUMBER PLAYING ----------------')
 
+#Positives
+
 numint = 10
 numlong = 3685974589764576458976457
 numfloat = 3.1415926535897932384626433835729
@@ -35,6 +37,8 @@ numexp2 = pow(4,10)
 numsci = 97439e12
 
 print(numint,numlong,numfloat,numfloat2,numcomp,numexp1,numexp2,numsci)
+
+#Negatives
 
 nnumint = -10
 nnumlong = -3685974589764576458976457
@@ -47,14 +51,41 @@ nnumsci = -97439e12
 
 print(nnumint,nnumlong,nnumfloat,nnumfloat2,nnumcomp,nnumexp1,nnumexp2,nnumsci)
 
-print(12*84/7-9)
-print( (
+
+# Order of operations. 
+
+print(12 * 84/ 7 - 9)
+print( ( (3**2 + 5) * 3 ) - 6 + 5 )
+print( ( ( 84 / 7 )**2 - 9) * 4 + 4)
+print( (8*2**2 - 8) - 2 * 10 )
+print( 3 - 10 * (4-3)**3 + 10)
+
+# Basic operations. 
+print(--numint)
+print(-------numint)
+
+
+
+
+# Various arithmetic using a single variable. 
 
 print(numint + numint)
 print(numint / (numint - numint + 1))
 print(int( numint / (numint - numint + 1) ))
 print(numint % 3)
 print(numint % numint)
+
+
+# Implicit definition
+
+numint2 = numint + 1
+
+print(numint2)
+
+print(numfloat + 2 / numint + numint2 - 43)
+
+
+
 
 
 
@@ -65,14 +96,21 @@ string2 = 'Yes Ariel? What is Nic doing?'
 string3 = 'Umm... I "dunno". '
 string4 = 'He\'s doing other stuff'
 binarynum = '01010011'
+binarynum2 = '00101100'
 hexnum = '0xff'
 
 print(int(binarynum,2))
+print(int(binarynum2,2))
 print(int(hexnum,16))
 print(int('1f',16))
 print(int('0x1f',16))
 
 print(string4)
+print(string1[1:5])
+print(string1[10:])
+print(string1*2)
+print(string3*len(string3))
+print(string3*(len(string3) - 4))
 
 
 print('----------------- LIST PLAYING ----------------')
