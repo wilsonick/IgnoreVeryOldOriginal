2f18b5befe16b605b53cdc3b30f2490a58bd80c5
Nicholas <blockpit@gmail.com>
2018-04-27 22:11:30 +1000

    Add files via upload

---
+++ fifth.py
@@ -0,0 +1,36 @@
+print('----------------- COMBINED DATA TYPE PLAYING ----------------')
+
+# Strings of numbers
+
+
+
+# Lists of a certain number of strings
+
+
+
+# Lists of strings and numbers
+
+
+
+# List of tuples
+
+
+
+# Sets of strings and numbers
+
+
+
+# Tuples of strings
+
+
+
+# Dictionaries of lists
+
+
+# Dictionaries of lists of numbers and strings
+
+
+# Dictionaries of tuples
+
+
+
