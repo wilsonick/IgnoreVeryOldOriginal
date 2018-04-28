44e8772dedea6eb5679086849521fce5fd7ea13a
Nicholas <blockpit@gmail.com>
2018-04-28 17:11:35 +1000

    Add files via upload

--- python/fifth.py
+++ python/fifth.py
@@ -1,2 +1,60 @@
-print('----------- 2 EMBEDDED DATA TYPES PLAYING -------------')
+print('----------------- COMBINED DATA TYPE PLAYING ----------------')
+
+# Strings of numbers
+string1 = '56'
+string2 = '1248163264128'
+string3 = '12''13'
+string4 = '13'' - ''12'
+string5 = '19 - ''12=7'
+string6 = "120""240"
+string7 = "150"'4000'
+
+print(string1, string2, string3, string4, string5, string6, string7)
+
+print(string2[0:1],string2[2:3],string2[3:4],string2[4:6],string2[6:8])
+
+'''
+# Try to automate a printing of strings containing numbers. 
+for i in (5*['string'])+[1,2,3,4,5]:
+    print(i)
+'''
+
+# Lists of a certain number of strings
+stringlist1 = ['hello','everybody']
+stringlist2 = ['super','kala','fragi','listic','expi','ala','docious']
+
+print(stringlist1,stringlist2,stringlist1+stringlist2)
+
+# Lists of strings and numbers
+liststrnum1 = ['Hello',100,'people',200,'of you are cool']
+liststrnum2 = ['Always',30,000,'people']
+liststrnum3 = ['100','100,000','people''is','what','you want']
+
+print(liststrnum1,liststrnum2,liststrnum3)
+
+for i in range(len(liststrnum1)):
+    if not (isinstance(liststrnum1[i],str)):
+        print('These are numbers:',liststrnum1[i])
+
+# List of tuples
+
+
+
+# Sets of strings and numbers
+
+
+
+# Tuples of strings
+
+
+
+# Dictionaries of lists
+
+
+# Dictionaries of lists of numbers and strings
+
+
+# Dictionaries of tuples
+
+
 
